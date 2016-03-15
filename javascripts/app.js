d793bfabef6af6ee3de3aeefa0c84eebf96225f8
shupac800 <davidshuman@gmail.com>
2016-03-15 15:23:24 -0500

    progress on search function

---
+++ javascripts/app.js
@@ -0,0 +1,25 @@
+var app = angular.module("movieHistory", ['ngRoute']);  // ngRoute object from angular-route is dependency -- needed to run
+
+app.config(['$routeProvider',  // $routeProvider object is given to us by ngRoute
+  function($routeProvider) {
+    $routeProvider.  // note similarity of this syntax to switch/case
+      when('/', {
+        templateUrl: 'partials/root.html',
+        controller: 'RootCtrl'
+      }).
+      when('/findNew', {
+        templateUrl: 'partials/find-new.html',
+        controller: 'FindNewCtrl'
+      }).
+      when('/searchYour', {
+        templateUrl: 'partials/search-your.html',
+        controller: 'SearchYourCtrl'
+      }).
+      when('/login', {
+        templateUrl: 'partials/login.html',
+        controller: 'LoginCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ javascripts/controllers/root-ctrl.js
@@ -0,0 +1,39 @@
+app.controller("RootCtrl", ["$scope",
+  function() {
+    console.log("RootCtrl is running");
+  }
+]);
+
+// ,"$http","song-storage",
+
+//   function($scope,$http,songStorage) {
+//     var doPost = function() {
+//       $http({
+//         url: "https://sizzling-heat-8987.firebaseio.com/songs/.json",
+//         method: "POST",
+//         data: JSON.stringify({
+//           id:  $scope.songs.length,
+//           name: $scope.songName,
+//           artist: $scope.artistName,
+//           album: $scope.albumName
+//         })
+//       }).then(function() {
+//         console.log("posted",$scope.songName);
+//       });
+//     };
+//     $scope.addStuff = function() {
+//       songStorage.then(function(songArray) {
+//         $scope.songs = songArray;
+//         $scope.songs.push({
+//           id:  $scope.songs.length + 1,
+//           name: $scope.songName,
+//           artist: $scope.artistName,
+//           album: $scope.albumName
+//         });
+//         doPost();
+//       });
+//       console.log("added");
+//     };
+//   }
+
+// ]);
