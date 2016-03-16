dc0567ac2f9aee969484eec29262cc1badff3a23
Palmer Harrell <palmerharrell@me.com>
2016-03-16 14:37:27 -0500

    Replaced firebase URL with our firebase URL

--- javascripts/factories/load-collection.js
+++ javascripts/factories/load-collection.js
@@ -4,7 +4,7 @@ app.factory("load-collection", function($q, $http) {
   function getCollection() {
     return $q(function(resolve, reject) {
       $http({  // $http returns a promise
-        url: "https://pizzapaperairplane.firebaseio.com/movies/.json",
+        url: "https://mb-movie-history.firebaseio.com/movies/.json",
         method: "GET"
       }).
       then(
