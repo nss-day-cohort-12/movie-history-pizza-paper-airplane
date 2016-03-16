9368edc55b1a46472b803335068e98314eccbc8d
duggerNot <dugger.jk@gmail.com>
2016-03-16 12:50:46 -0500

    added login functionality

--- javascripts/controllers/login-ctrl.js
+++ javascripts/controllers/login-ctrl.js
@@ -1,5 +1,56 @@
-app.controller("LoginCtrl", ["$scope",
-  function() {
-    console.log("LoginCtrl controller is running");
+app.controller("LoginCtrl",
+[
+  "$scope",
+  "$location",
+  "$http",
+  "authFactory",
+  "firebaseURL",
+
+  function($scope, $location, $http, authFactory, firebaseURL) {
+
+    // Local variables
+    let ref = new Firebase(firebaseURL);
+
+    // Variables on $scope for use in DOM
+    $scope.account = { email: "", password: "" };
+    $scope.message = "";
+
+    // Unauthenticate user when /logout path used
+    if ($location.path() === "/logout") {
+      ref.unauth();
+    }
+
+    /*
+      Attempt to register a new user account.
+      If successful, immediately log user in.
+     */
+    $scope.register = () => {
+      ref.createUser({
+        email    : $scope.account.email,
+        password : $scope.account.password
+      }, (error, userData) => {
+        if (error) {
+          console.log(`Error creating user: ${error}`);
+        } else {
+          console.log(`Created user account with uid: ${userData.uid}`);
+          $("span#regSpan").html("User Registered!");
+          $scope.login();
+        }
+      });
+    };
+
+    /*
+      Attempt to authenticate the user with the
+      supplied credentials.
+     */
+    $scope.login = () => 
+      authFactory
+        .authenticate($scope.account)
+        .then(() => {
+          $location.path("/");
+          $scope.$apply();  // Needed for $location.path() to succeed
+        });
+
+
   }
 ]);
